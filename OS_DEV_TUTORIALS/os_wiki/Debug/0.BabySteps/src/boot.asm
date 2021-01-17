d7fc7c122cf47ff61232bcaf5c3d7dd0cb19311a
Denchidlo <twihkapb@gmail.com>
2021-01-17 22:17:32 +0300

    baby step 2

--- OS_DEV_TUTORIALS/os_wiki/Debug/0.BabySteps/src/boot.asm
+++ OS_DEV_TUTORIALS/os_wiki/Debug/0.BabySteps/src/boot.asm
@@ -1,10 +1,18 @@
-;   boot1.asm
-cli
-
-hang_loop:
-    jmp hang_loop
-
-times 510-($-$$) db 0
-
-db 0x55
-db 0xAA
+[ORG 0x7c00]
+   xor ax, ax  ;make it zero
+   mov ds, ax
+   cld
+ 
+   mov si, msg
+   call bios_print
+ 
+hang:
+   jmp hang
+ 
+msg   db 'Hello World', 13, 10, 0
+ 
+%include "proc.inc"
+ 
+   times 510-($-$$) db 0
+   db 0x55
+   db 0xAA
